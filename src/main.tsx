d660fe34b65a228b25614ea9fd5c613bab917ea4
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-10-12 23:06:25 +0300

    chore: add СSS variables

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client'
 
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>

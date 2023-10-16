9af6bb854b1e7c2dd31eaf13c83ad1b4b5de404a
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-10-16 21:49:37 +0300

    add eye-off-outline svg

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,2 +1,3 @@
+export { default as EyeOffOutlineIcon } from '@/assets/icons-components/EyeOffOutlineIcon'
 export { default as EyeOutlineIcon } from '@/assets/icons-components/EyeOutlineIcon'
 export { default as PersonOutlineIcon } from '@/assets/icons-components/PersonOutlineIcon'

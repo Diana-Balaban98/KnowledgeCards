c4f4cca897251b833096714e53ee5d8e4eb226c4
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-10-20 02:23:47 +0300

    add styles for checkbox

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,6 +1,7 @@
 import { FC, useId } from 'react'
 
-import { CheckIcon } from '@/assets'
+import CheckIcon from '@/assets/icons-components/CheckIcon'
+import { Typography } from '@/components'
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
 import * as LabelRadix from '@radix-ui/react-label'
 
@@ -19,20 +20,20 @@ export const Checkbox: FC<CheckBoxProps> = ({ onChange }) => {
 
   return (
     <div className={s.containerCheckBox}>
-      <div className={s.buttonContainer}>
+      <div className={s.buttonContainer} tabIndex={Number('0')}>
         <CheckboxRadix.Root
-          className={'CheckboxRoot'}
+          className={s.checkboxRoot}
           defaultChecked
           id={uid}
           onCheckedChange={onChange}
         >
-          <CheckboxRadix.Indicator className={'CheckboxIndicator'}>
+          <CheckboxRadix.Indicator className={s.checkIcon}>
             <CheckIcon />
           </CheckboxRadix.Indicator>
         </CheckboxRadix.Root>
       </div>
       <LabelRadix.Root className={s.label} htmlFor={uid}>
-        Check-box
+        <Typography variant={'body2'}>Check-box</Typography>
       </LabelRadix.Root>
     </div>
   )

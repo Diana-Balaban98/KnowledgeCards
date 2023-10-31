64b854db6b5865ba68993e6ab7bf2b632ebe3def
ILIABARABANOV <ilianardan@gmail.com>
2023-10-31 17:32:59 +0300

    fixed: linters

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,52 +1,52 @@
-import {ElementRef, forwardRef, useState} from 'react'
+import { ElementRef, forwardRef, useState } from 'react'
 
-import * as RadixSlider from '@radix-ui/react-slider';
+import SliderValue from '@/components/ui/slider/sliderValue'
+import * as RadixSlider from '@radix-ui/react-slider'
+
 import s from './slider.module.scss'
-import SliderValue from "@/components/ui/slider/SliderValue.tsx";
 
 export type SliderProps = Partial<{
-    className: string,
-    defaultValue: number[],
-    max: number,
-    step: number,
-    minStepsBetweenThumbs: number,
-
-
+  className: string
+  defaultValue: number[]
+  max: number
+  minStepsBetweenThumbs: number
+  step: number
 }>
 
-export const Slider = forwardRef<ElementRef<typeof RadixSlider.Root>, SliderProps>((
-        {className, defaultValue = [0, 10], max, step = 1, minStepsBetweenThumbs , ...restProps}, _ref
-    ) => {
+export const Slider = forwardRef<ElementRef<typeof RadixSlider.Root>, SliderProps>(
+  (
+    { className, defaultValue = [0, 10], max, minStepsBetweenThumbs, step = 1, ...restProps },
+    _ref
+  ) => {
+    const [currentValue, setCurrentValue] = useState<number[]>(defaultValue)
 
-        const [currentValue, setCurrentValue] = useState<number[]>(defaultValue)
-
-
-        const onValueChange = (value: number[]) => {
-            setCurrentValue(value)
-        }
-
-        return (
-
-            <span className={s.SliderContainer}>
-                <SliderValue value={currentValue[0]}/>
-
-                <RadixSlider.Root value={currentValue} onValueChange={onValueChange} className={`${s.SliderRoot}`}
-                                  defaultValue={defaultValue}
-                                  max={max} step={step} minStepsBetweenThumbs={1} {...restProps}>
-                    <RadixSlider.Track className={s.SliderTrack}>
-                        <RadixSlider.Range className={s.SliderRange}/>
-                    </RadixSlider.Track>
-                    <RadixSlider.Thumb className={s.SliderThumb} aria-label="Volume"/>
-                    <RadixSlider.Thumb className={s.SliderThumb} aria-label="Volume"/>
-
-                </RadixSlider.Root>
-
-            <SliderValue value={currentValue[1]}/>
-        </span>
-
-
-        )
+    const onValueChange = (value: number[]) => {
+      setCurrentValue(value)
     }
+
+    return (
+      <span className={s.sliderContainer}>
+        <SliderValue value={currentValue[0]} />
+
+        <RadixSlider.Root
+          className={`${s.sliderRoot}`}
+          defaultValue={defaultValue}
+          max={max}
+          minStepsBetweenThumbs={1}
+          onValueChange={onValueChange}
+          step={step}
+          value={currentValue}
+          {...restProps}
+        >
+          <RadixSlider.Track className={s.sliderTrack}>
+            <RadixSlider.Range className={s.SliderRange} />
+          </RadixSlider.Track>
+          <RadixSlider.Thumb aria-label={'Volume'} className={s.sliderThumb} />
+          <RadixSlider.Thumb aria-label={'Volume'} className={s.sliderThumb} />
+        </RadixSlider.Root>
+
+        <SliderValue value={currentValue[1]} />
+      </span>
+    )
+  }
 )
-
-

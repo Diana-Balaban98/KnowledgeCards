22a6e3b40a872e8558589e43a463357e1ed3601f
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-11-02 21:54:32 +0300

    refactor code in dropdown

--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -1,33 +1,70 @@
-import { ElementRef, ReactElement, forwardRef } from 'react'
+import { CSSProperties, ElementRef, ReactElement, forwardRef } from 'react'
 
+import { MoreIcon } from '@/assets'
+import { Typography } from '@/components'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { clsx } from 'clsx'
 
 import s from './dropdown.module.scss'
 
 type DropdownProps = {
+  align?: 'center' | 'end' | 'start'
+  ariaLabel?: string
+  className?: string
+  heightTrigger?: CSSProperties['height']
+  items: {
+    icon: ReactElement
+    label: string
+    onClick: () => void
+  }[]
   trigger?: ReactElement
+  widthTrigger?: CSSProperties['width']
 } & DropdownMenu.DropdownMenuProps
 
 export const Dropdown = forwardRef<ElementRef<typeof DropdownMenu.Root>, DropdownProps>(
-  ({ trigger }, ref) => {
+  ({ align = 'start', ariaLabel, className, heightTrigger, items, trigger, widthTrigger }, ref) => {
+    const classNames = {
+      box: s.box,
+      btn: s.btn,
+      dropdownContent: clsx(s.dropdownContent, className),
+      dropdownItem: s.dropdownItem,
+      dropdownSeparator: s.dropdownSeparator,
+      trigger: s.trigger,
+    }
+
+    const sizeTrigger = { height: heightTrigger, width: widthTrigger }
+
     return (
       <div className={s.box}>
         <DropdownMenu.Root>
-          <DropdownMenu.Trigger asChild>
-            <button aria-label={'Customise options'} className={'IconButton'}>
-              {trigger}
+          <DropdownMenu.Trigger className={classNames.trigger}>
+            <button
+              aria-label={ariaLabel ?? 'Customise options'}
+              className={classNames.btn}
+              style={sizeTrigger}
+            >
+              {trigger ?? <MoreIcon />}
             </button>
           </DropdownMenu.Trigger>
-
           <DropdownMenu.Portal>
-            <DropdownMenu.Content className={s.dropdownMenuContent} ref={ref}>
-              <DropdownMenu.Item className={s.DropdownMenuItem}>New Tab</DropdownMenu.Item>
-              <DropdownMenu.Item className={s.DropdownMenuItem}>New Window</DropdownMenu.Item>
-              <DropdownMenu.Item className={s.DropdownMenuItem}>
-                New Private Window
-              </DropdownMenu.Item>
-              <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
-              <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
+            <DropdownMenu.Content
+              // align={align}
+              className={classNames.dropdownContent}
+              onClick={e => e.stopPropagation()}
+              ref={ref}
+              side={'bottom'}
+              sideOffset={15}
+            >
+              {items.map((item, index) => (
+                <DropdownMenu.Item
+                  className={classNames.dropdownItem}
+                  key={index}
+                  onSelect={item.onClick}
+                >
+                  {item.icon}
+                  <Typography variant={'caption'}>{item.label}</Typography>
+                </DropdownMenu.Item>
+              ))}
             </DropdownMenu.Content>
           </DropdownMenu.Portal>
         </DropdownMenu.Root>

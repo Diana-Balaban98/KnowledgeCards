819943b57aa4ac40daa2384bbd76db564812a5c8
ILIABARABANOV <ilianardan@gmail.com>
2023-11-03 03:11:44 +0300

    feat: modal with universal props

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -6,47 +6,45 @@ import { Cross2Icon } from '@radix-ui/react-icons'
 
 import s from './modal.module.scss'
 
-export type ModalProps = Partial<{
-  className: string
+export type ModalProps = {
+  open?: boolean
+  setOpen?: (open: boolean) => void
   title: string
   trigger: ReactNode
-}> &
-  ComponentPropsWithoutRef<'div'>
+} & ComponentPropsWithoutRef<'div'>
 
 export const Modal = forwardRef<ElementRef<typeof RadixModal.Root>, ModalProps>(
-  ({ children, title, trigger }, _ref): JSX.Element => {
+  ({ children, className, title, trigger, ...restProps }, ref): JSX.Element => {
     const [open, setOpen] = useState(false)
 
     return (
-      <div>
-        <RadixModal.Root onOpenChange={setOpen} open={open}>
-          <RadixModal.Trigger asChild>{trigger}</RadixModal.Trigger>
-          {open && (
-            <RadixModal.Portal forceMount>
-              <RadixModal.Overlay className={`${s.RadixModalOverlay}`} forceMount />
-              <RadixModal.Content className={`${s.RadixModalContent}`}>
+      <RadixModal.Root onOpenChange={setOpen} open={open}>
+        <RadixModal.Trigger asChild>{trigger}</RadixModal.Trigger>
+
+        {open && (
+          <RadixModal.Portal forceMount>
+            <RadixModal.Overlay className={s.overlay} forceMount />
+
+            <div className={s.root} ref={ref} {...restProps}>
+              <RadixModal.Content asChild forceMount>
                 <Card>
-                  <header className={s.Fieldset}>
+                  <header className={s.header}>
                     <Typography as={'h2'} variant={'h2'}>
                       {title}
                     </Typography>
+                    <RadixModal.Close asChild>
+                      <button aria-label={'Close'} className={s.IconButton}>
+                        <Cross2Icon />
+                      </button>
+                    </RadixModal.Close>
                   </header>
                   <div>{children}</div>
                 </Card>
-                <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 25 }}>
-                  <RadixModal.Close asChild>
-                    <button className={`${s.Button}${s.green}`}>Save changes</button>
-                  </RadixModal.Close>
-                </div>
-                <RadixModal.Close asChild>
-                  <Cross2Icon />
-                  <button aria-label={'Close'} className={`${s.IconButton}`}></button>
-                </RadixModal.Close>
               </RadixModal.Content>
-            </RadixModal.Portal>
-          )}
-        </RadixModal.Root>
-      </div>
+            </div>
+          </RadixModal.Portal>
+        )}
+      </RadixModal.Root>
     )
   }
 )
